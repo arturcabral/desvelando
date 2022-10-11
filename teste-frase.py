00e1728098153dca21a4d218a1b1307157a0a691
arturcabral <arturcabralreis@gmail.com>
2022-10-11 16:40:57 -0300

    to do

--- teste-frase.py
+++ teste-frase.py
@@ -1,3 +1,5 @@
+#verificar acentos
+
 r1 = "Bela Vista, 25 de março de 1966\n\n\nInesquecível Silvestre \n\n\nRecebi dia 23 sua carta do dia 13. Já estava preocupada sem notícias sua, pensei até que já estivesse me esquecido. Mas fiquei bastante contente com sua cartinha que por umas horas desapareceu está tristeza infinita que consome meus dias. Para mim, aqui sem você tudo é triste, por mais que eu procure fazer qualquer serviço para me distrair, cuidar das plantas, dos coelhos, sinto ainda mais saudades sua. Já quero que esteja ao meu lado. Já estava mesmo em falta com você, mas sei que você tem um bom coração, abuso de sua bondade como sei que vai me perdoar."
 
 import Adafruit_BBIO.GPIO as GPIO
